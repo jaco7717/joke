60231dd1d082bcdb61df0c36235d109f89961067
Henrik de Laurent Andersen <henrikdelaurent@outlook.dk>
2019-04-09 15:29:41 +0200

    alle jokes

---
+++ public/alleJokes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const handlebars = express();
+const fetch = require('node-fetch');
+
+handlebars.use(express.static('public'));
+
+handlebars.get('/public', function(request,response) {
+    fetch('https://jokservice.herokuapp.com/api/jokes').then (result => {response.json(result)})
+
+});
+
+handlebars.get('/', function (request, response) {
+    response.sendStatus(404);
+})
+
+.listen(8080);
+
