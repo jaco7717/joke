55bcb3fc7f796ff22b07d9680ee624c4909d051f
JacobVB <jaco7717@gmail.com>
2019-04-09 15:31:25 +0200

    ddd

--- joke.js
+++ joke.js
@@ -27,7 +27,7 @@ const messageSkema = new Schema({
 
 const messageModel = mongoose.model('message', messageSkema);
 
-handlebars.use(express.static('public'));
+//handlebars.use(express.static('public'));
 
 
 
