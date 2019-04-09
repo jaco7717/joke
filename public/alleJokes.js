831ccf96bd1f6c777a6355bc1bd5345915e569a0
JacobVB <jaco7717@gmail.com>
2019-04-09 16:00:25 +0200

    fff

--- public/alleJokes.js
+++ public/alleJokes.js
@@ -3,7 +3,7 @@ const handlebars = express();
 const fetch = require('node-fetch');
 
 handlebars.use(express.static('public'));
-let PORT = process.env.PORT || 8080;
+
 handlebars.get('/public', function(request,response) {
     fetch('https://jokservice.herokuapp.com/api/jokes').then(result => {response.json(result)})
 
@@ -11,6 +11,6 @@ handlebars.get('/public', function(request,response) {
 
 handlebars.get('/', function (request, response) {
     response.sendStatus(404);
-}).listen(PORT);
+});
 
 
