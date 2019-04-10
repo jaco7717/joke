7e19fe3e17384379f63a52125e826d5b4a97eb56
JacobVB <jaco7717@gmail.com>
2019-04-10 12:22:38 +0200

    test

--- public/index.js
+++ public/index.js
@@ -1,18 +1,11 @@
 
-
-
 update();
-
-
-
-
-
 
 async function update() {
     document.querySelector('#jokes').innerHTML = '';
     for (let input of document.querySelectorAll('input')) input.value = '';
     getJokes();
- 
+
 
 }
 
@@ -22,6 +15,8 @@ async function getJokes() {
     const templeteText = await  template.text();
     const jokes = await userResponse.json();
     const compiledTemplate = Handlebars.compile(templeteText);
-    document.querySelector('#messages').innerHTML = compiledTemplate({jokes});
+    document.querySelector('#jokes').innerHTML = compiledTemplate({jokes});
 }
+
+
 
