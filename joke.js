20919ac73dad8b5b0cbea6d058a296ac3254de36
JacobVB <jaco7717@gmail.com>
2019-04-10 11:56:50 +0200

    lorte jokes

--- joke.js
+++ joke.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 
  onload = async () => {
      const [template, response] =
-         await Promise.all([fetch('/jokes.hbs'), fetch('localhost:8080/api/jokes')]);
+         await Promise.all([fetch('/jokes.hbs'), fetch('https://jokservice.herokuapp.com/api/jokes')]);
      const templateText = await template.text();
      const messages = await response.json();
      const compiledTemplate = Handlebars.compile(templateText);
