b9451ae3809d93af99d3c00675f908deb3b0e5d8
Henrik de Laurent Andersen <henrikdelaurent@outlook.dk>
2019-04-10 09:21:33 +0200

    udkommenter

--- public/joke.js
+++ public/joke.js
@@ -14,14 +14,14 @@ mongoose.Promise = Promise;
 mongoose.connect('mongodb+srv://emillouvmand:UXRYzVDa1nR7kHzX@jacob-otbax.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
 
 
-onload = async () => {
-    const [template, response] =
-        await Promise.all([fetch('/jokes.hbs'), fetch('/api/jokes')]);
-    const templateText = await template.text();
-    const messages = await response.json();
-    const compiledTemplate = handlebars.compile(templateText);
-    document.body.innerHTML = compiledTemplate({messages});
-};
+// onload = async () => {
+//     const [template, response] =
+//         await Promise.all([fetch('/jokes.hbs'), fetch('/api/jokes')]);
+//     const templateText = await template.text();
+//     const messages = await response.json();
+//     const compiledTemplate = handlebars.compile(templateText);
+//     document.body.innerHTML = compiledTemplate({messages});
+// };
 
 
 
